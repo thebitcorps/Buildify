8e2272f993cb927a997c3ff8f2be6f276e9b4c37
David De Anda <omarandstuff@gmail.com>
2015-08-19 23:04:15 -0500

    Bootstrap and root

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem "therubyracer"
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  resources :constructions, only: [:new, :create, :index, :show]
-  resources :materials, only: [:new, :create, :index]
-  resources :providers, only: [:new, :create, :index]
-  resources :requisitions, only: [:create, :index, :show] do
-    post 'generate_purchase_order'
-  end
-  resources :item_materials, only: [:create]
-  resources :purchase_orders, only: [:update, :show] do
-    patch 'update_item_materials'
-  end
-  resources :expenses, only: [:update, :index]
-  resources :invoice_receipts, only: [:create, :show, :update]
-  resources :invoices, only: [:create, :show] do
-    post 'add_purchase_order'
-    post 'remove_purchase_order'
-  end
+  root to: "home#index"
 end
