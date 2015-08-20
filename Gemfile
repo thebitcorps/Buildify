986be6b0a2052e1909a9c48e7bf61a02c8b29214
Diego Llamas <diegollams@gmail.com>
2015-08-20 00:49:05 -0500

    add user and devise basic

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
 gem "twitter-bootstrap-rails"
 
+gem 'devise'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
-Construction.create(title: "Kinder", contract_amount: 4000000)
-Construction.create(title: "Escuela", contract_amount: 4000000)
-Construction.create(title: "Prepa", contract_amount: 4000000)
-
-Material.create(name: "Madera", description: "10x10")
-Material.create(name: "Madera", description: "20x120")
-Material.create(name: "Semento", description: "Gris")
-Material.create(name: "Cal", description: "Blanca")
-Material.create(name: "Ladrillo", description: "Naranja")
-Material.create(name: "Tornillo", description: "12")
-Material.create(name: "Clavo", description: "10")
+User.create({name: "The bit corps",email: "dev@thebitcorps.com",password: "12345678",password_confirmation: "12345678"})
 
 
-Provider.create(name: "Aceros Rodriguez", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "acerosr@correo.com")
-Provider.create(name: "Cemex", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "Cemex@correo.com")
-Provider.create(name: "Comex", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "Comex@correo.com")
-Provider.create(name: "Materiales Vasques", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "vasques@correo.com")
+# Construction.create(title: "Kinder", contract_amount: 4000000)
+# Construction.create(title: "Escuela", contract_amount: 4000000)
+# Construction.create(title: "Prepa", contract_amount: 4000000)
+#
+# Material.create(name: "Madera", description: "10x10")
+# Material.create(name: "Madera", description: "20x120")
+# Material.create(name: "Semento", description: "Gris")
+# Material.create(name: "Cal", description: "Blanca")
+# Material.create(name: "Ladrillo", description: "Naranja")
+# Material.create(name: "Tornillo", description: "12")
+# Material.create(name: "Clavo", description: "10")
+#
+#
+# Provider.create(name: "Aceros Rodriguez", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "acerosr@correo.com")
+# Provider.create(name: "Cemex", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "Cemex@correo.com")
+# Provider.create(name: "Comex", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "Comex@correo.com")
+# Provider.create(name: "Materiales Vasques", address: "Calle Falsa #123", telephone: "9-10-10-10", email: "vasques@correo.com")
