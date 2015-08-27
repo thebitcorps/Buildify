216ea5a930c577aa92bca658f73405a5c3045379
vzqzac <omar.vazquezac@gmail.com>
2015-08-27 07:45:42 -0500

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,33 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   ROLES = ['administrator','resident']
-  validates :name,:phone,:role, presence: true
+  validates :name,:phone,:role, :email, presence: true
+  validates_format_of :name, :with => /[a-z]/, message: 'Ingresar sólo letras'
+  validates :email, format: {
+            with: %r{(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\Z}i,
+            message: 'email invotra cosalido'
+                  }
+  # validate :validate_phone_number
 #   validate :phone ,check which regex uses
   validates :phone, format: {
-                with: %r{\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d\Z}i,
-                message: 'Invalid format'
-                  } #no sirve
+               with: %r{\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d\Z}i,
+               message: 'Invalid format'
+                   } # no sirve
   validates :role, inclusion: ROLES
   paginates_per 10
 
   scope :resident, -> {where role: 'resident'}
   scope :administrator, -> {where role: 'administrator'}
 
+  # def validate_phone_number
+  #   if (:phone.length == 10) or (:phone.length == 12 && (:phone[0,2] == '55' or :phone[0,2] == '33' or :phone[0,2] == '88'))
+  #     validates :phone, format: {with: %r{\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d\Z}i}
+  #   else if :phone.length == 12 && (:phone[0,2] == '55' or :phone[0,2] == '33' or :phone[0,2] == '88')
+  #     validates :phone[2,12], format: {with: %r{\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d\Z}i}
+  #        else errors.add(:phone, "Teléfono inválido")
+  #        end
+  #   end
+  # end
   def self.search(search)
     return all if search.nil?
     unless search.empty?
@@ -25,6 +40,4 @@ class User < ActiveRecord::Base
       all
     end
   end
-
-
 end
