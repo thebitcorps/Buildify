b4efad673602f32d5a650e70c2f69471d49dbc59
David De Anda <omarandstuff@gmail.com>
2015-09-19 11:20:34 -0500

    Providers scaffold

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,13 +1,10 @@
 class PurchaseOrdersController < ApplicationController
 
-
-
   def new
     @purchase_oder = PurchaseOrder.new
     requisition = Requisition.find(params[:requisition_id])
     @requisition = JSON.parse requisition.to_json include: [:item_materials => {include: :material}]
     @construction_address = 'requisition.construction.name.to_json'
   end
-
 
 end
