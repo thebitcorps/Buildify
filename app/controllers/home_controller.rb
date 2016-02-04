97e719a6150a2be8f50a1b329c0d367a9c53e015
Diego Llamas <diegollams@gmail.com>
2016-02-04 08:47:13 -0600

    add balances to home administrator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,20 @@ class HomeController < ApplicationController
   def index
     @constructions = Construction.running
     @payments = Payment.all
+    @balance = get_expenses(@constructions)
+  end
+  private
+  # method that extract all the expenses from the given construction array
+  # return: hash with keys expeneses and paid with the sum of all the expeneses and paid
+  def get_expenses(constructions)
+    information = {}
+    information[:expenses] = 0
+    information[:paid] = 0
+    constructions.each do |construction|
+      information[:expenses] += construction.expenses
+      information[:paid] += construction.paid
+    end
+    information
   end
 
 end
