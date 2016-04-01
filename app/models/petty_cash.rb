f4699f404bde8f37e92e0f4f892f96e6892f8904
Diego Llamas <diegollams@gmail.com>
2016-04-01 12:37:27 -0600

    bug in create petty cash

--- app/models/petty_cash.rb
+++ app/models/petty_cash.rb
@@ -7,13 +7,13 @@ class PettyCash < ActiveRecord::Base
 
   before_create :close_petty_cash
 
-  scope :active_from_construction, ->(construction_id) { where(construction_id: construction_id).order(created_at: :asc).last}
+  scope :active_from_construction, ->(construction_id) { where(construction_id: construction_id).order(created_at: :asc).last }
   scope :with_construction, ->(construction_id) { where(construction_id: construction_id).order(created_at: :desc)}
 
   # close last last petty cash before from a construction
   def close_petty_cash
     last_petty_chash = PettyCash.active_from_construction self.construction_id
-    return if last_petty_chash.empty?
+    return if last_petty_chash.nil?
     last_petty_chash.closing_date = Time.now
     last_petty_chash.save
   end
