bad9480d0a6c82cabc2506f2cd0492940372b8d2
Roberto Ruiz Velasco Martinez <rob@thebitcorps.com>
2017-02-13 11:28:57 -0600

    chmh recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,27 +4,27 @@ lock "3.7.2"
 # Change these
 server '107.170.200.55', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:thebitcorps/Buildify.git'
-set :application,     'Buildify'
-set :user,            'deployer'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :repo_url, 'git@github.com:thebitcorps/Buildify.git'
+set :application, 'Buildify'
+set :user, 'deployer'
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :pty, true
+set :use_sudo, false
+set :stage, :production
+# set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options, {forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)}
 set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+# set :puma_worker_timeout, nil
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -36,6 +36,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{.env.production}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -76,10 +77,10 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :restart
 end
 
 # ps aux | grep puma    # Get puma pid
